981a92a0e1517482351148f34a929255bd9ce00c
micsamuel <mykelsown@gmail.com>
2026-03-03 17:07:29 +0000

    gs

--- endpoint/main.go
+++ endpoint/main.go
@@ -7,13 +7,17 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("The input format should be [go run maing.go <inputfile-name> <outputfile-name>]")
+		return
+	}
+
 	input, _ := os.Open(os.Args[1])
 	defer input.Close()
-	mainInKey := bufio.NewScanner(input)
 	output, _ := os.Create(os.Args[2])
 	defer output.Close()
+	mainInKey := bufio.NewScanner(input)
 	// mainOut := bufio.NewWriter(output)
 	mainIn := mainInKey.Text()
-
 	fmt.Printf("%v", mainIn)
 }
