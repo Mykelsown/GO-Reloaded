1aadb3c947297fc93333bc65e26188997d8fd2e1
micsamuel <mykelsown@gmail.com>
2026-03-05 14:47:26 +0000

    Handled cases for hexadecimals, binaries, lowercase, uppercase and capitalization

---
+++ HexAndBin.go
@@ -0,0 +1,37 @@
+package GoReloaded
+
+import (
+	"regexp"
+	"strconv"
+)
+
+var re = regexp.MustCompile(`([0-9A-Fa-f]+)\s*\((hex|bin)\)`)
+
+func HexAndBin(s string) string {
+
+	result := re.ReplaceAllStringFunc(s, func(match string) string {
+
+		sub := re.FindStringSubmatch(match)
+
+		value := sub[1]
+		mode := sub[2]
+
+		var base int
+
+		switch mode {
+		case "hex":
+			base = 16
+		case "bin":
+			base = 2
+		}
+
+		num, err := strconv.ParseInt(value, base, 64)
+		if err != nil {
+			return match
+		}
+
+		return strconv.FormatInt(num, 10)
+	})
+
+	return result
+}
--- myLogic.go
+++ myLogic.go
@@ -1,5 +1,7 @@
 package GoReloaded
 
 func MyLogic(text string) string {
-	return text
+	final := HexAndBin(text)
+	final = LowUpAndCap(text)
+	return final
 }
